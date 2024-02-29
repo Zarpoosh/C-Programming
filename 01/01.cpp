9be9f78b7564e44d9eb6804ebcc541c13964951c
Zarpoosh <mzar67839@gmail.com>
2024-02-29 23:41:42 +0330

    first commite

---
+++ 01/01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+/*
+int main(){
+
+   
+    std::cout << "i like pizza!" <<std::endl;
+    std :: cout <<"its realy good :)" << '\n';
+    return 0;
+}
+*/
+
+int main(){
+
+   int x=5;   //decoleration
+   int y=3;    // assign ment
+    int sum=x+y; //pluss
+   std ::cout <<x << '\n';
+   std ::cout <<y << '\n';
+   std::cout<<sum << '\n'; //result
+
+    return 0;
+}
+
+//! new comment
